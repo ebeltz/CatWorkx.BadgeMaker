934603264a59f3bbf97eec80450ab8525e8390d9
ebeltz <ebbeltz@gmail.com>
2022-05-24 20:50:13 -0400

    add starter code for CatWorx

--- Program.cs
+++ Program.cs
@@ -6,9 +6,15 @@ namespace CatWorx.BadgeMaker
     {
         static void Main(string[] args)
         {
-            string greeting = "Hello";
-            greeting = greeting + "World";
-            Console.WriteLine("greeting" + greeting);
+            List<string> employees = new List<string>();
+	        Console.WriteLine("Please enter a name: ");
+	        // Get a name from the console and assign it to a variable
+	        string input = Console.ReadLine();
+	        employees.Add(input);
+	        for (int i = 0; i < employees.Count; i++) 
+	      {
+	        Console.WriteLine(employees[i]);
+	      }
         }
     }
 }
